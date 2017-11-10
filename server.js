7471c41042f07b007abf991626064b9504f2bb68
Donovon <donovonjenson@hotmail.com>
2017-11-10 11:18:31 -0800

    now serves static page

--- server.js
+++ server.js
@@ -4,10 +4,16 @@ var mongoose = require('mongoose');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 
-app.use(express.static(__dirname + '/hrsf84-mvp/index.html'))
+app.use(express.static(__dirname + '/public'))
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({'extended':'true'}));
 app.use(bodyParser.json());
 
 app.listen(8080);
 console.log('App listening on 8080')
+
+app.get('/', (req, res) => {
+	console.log('somethinghappened')
+	res.status(200);
+	res.sendfile('./public/index.html')
+})
